776b78888c8cb545d607adedf429a71c4dab4193
Fernanda Velazquez <fevelazq@gmail.com>
2020-03-30 22:49:55 -0600

    home screen added

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import HomeScreen from './src/screens/HomeScreen';
 import FanScreen from './src/screens/FanScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createStackNavigator();
 const Homebar = createBottomTabNavigator();
@@ -18,14 +17,6 @@ function Fan() {
   )
 }
 
-function Home() {
-  return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  )
-}
-
 export default function App() {
   return (
     <SafeAreaProvider>
@@ -34,20 +25,9 @@ export default function App() {
           <Homebar.Screen name="Fan">
             {() => Fan()}
           </Homebar.Screen>
-          <Homebar.Screen name="Home">
-            {() => Home()}
-          </Homebar.Screen>
+          <Homebar.Screen name="Home" component={HomeScreen} />
         </Homebar.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/PostsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import Wrapper from '../components/Wrapper';
+
+export default class PostsScreen extends React.Component {
+    render() {
+        return (
+            <Text>PostsScreen.js</Text>
+        )
+    }
+}
