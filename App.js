642059fb1af8314f704e6bafa957cb6743a80b1d
Fernanda Velazquez <fevelazq@gmail.com>
2020-03-31 16:53:19 -0600

    homebar icons added

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import FanScreen from './src/screens/FanScreen';
@@ -8,28 +7,49 @@ import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
 import RewardsScreen from './src/screens/RewardsScreen';
 import TeamScreen from './src/screens/TeamScreen';
+import { COLORS } from './src/styles';
+import { Icon } from "native-base";
 
-const Stack = createStackNavigator();
 const Homebar = createBottomTabNavigator();
 
-function Fan() {
-  return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name="Fan" component={FanScreen} />
-    </Stack.Navigator>
-  )
-}
+const TabBarIcon = ({ name, focused }) => (
+  <Icon name={name}
+    style={{
+      color: focused ? COLORS.TEAM_PRIMARY : COLORS.BLACK_BLUR
+    }}
+  />
+);
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Homebar.Navigator initialRouteName="Fan">
-          <Homebar.Screen name="Fan" component={FanScreen} />
-          <Homebar.Screen name="Matches" component={MatchesScreen} />
-          <Homebar.Screen name="Home" component={HomeScreen} />
-          <Homebar.Screen name="Rewards" component={RewardsScreen} />
-          <Homebar.Screen name="Team" component={TeamScreen} />
+          <Homebar.Screen name="Fan" component={FanScreen}
+            options={{
+              tabBarIcon: ({ focused }) => <TabBarIcon name="person" focused={focused} />
+            }}
+          />
+          <Homebar.Screen name="Matches" component={MatchesScreen}
+            options={{
+              tabBarIcon: ({ focused }) => <TabBarIcon name="football" focused={focused} />
+            }}
+          />
+          <Homebar.Screen name="Home" component={HomeScreen}
+            options={{
+              tabBarIcon: ({ focused }) => <TabBarIcon name="home" focused={focused} />
+            }}
+          />
+          <Homebar.Screen name="Rewards" component={RewardsScreen}
+            options={{
+              tabBarIcon: ({ focused }) => <TabBarIcon name="trophy" focused={focused} />
+            }}
+          />
+          <Homebar.Screen name="Team" component={TeamScreen}
+            options={{
+              tabBarIcon: ({ focused }) => <TabBarIcon name="shirt" focused={focused} />
+            }}
+          />
         </Homebar.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
