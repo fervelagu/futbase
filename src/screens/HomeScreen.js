a72f0dea06590d601adf58d9bc9c5faf8f6db4a2
Fernanda Velazquez <fevelazq@gmail.com>
2020-04-08 10:51:49 -0500

    packages version updated

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Wrapper } from '../components/Wrapper';
-import { Loading } from '../components/Loading';
 import { HeaderMatch } from '../components/HeaderMatch';
 import PostsScreen from './PostsScreen';
 import VideosScreen from './VideosScreen';
@@ -11,73 +10,70 @@ import StatisticsScreen from './StatisticsScreen';
 import CalendarScreen from './CalendarScreen';
 
 const Hometabs = createMaterialTopTabNavigator();
+const defaultMatch = {
+    tournament: "Liga MX",
+    stadium: "Estadio Azteca",
+    localAsset: "pumas",
+    local_url: null,
+    localTeam: "Pumas",
+    score: "0 - 0",
+    visitorAsset: "america",
+    visitor_url: null,
+    visitTeam: "América",
+    date: "Sábado 25, ABRIL"
+};
 
-function HomeTab({ state, descriptors, navigation }) {
-    return (
-        <View style={styles.tabStyle}>
-            {state.routes.map((route, index) => {
-                const { options } = descriptors[route.key];
-                const label =
-                    options.tabBarLabel !== undefined
-                        ? options.tabBarLabel
-                        : options.title !== undefined
-                            ? options.title
-                            : route.name;
+HomeTab = ({ state, descriptors, navigation }) => (
+    <View style={styles.tabStyle}>
+        {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
+            const label =
+                options.tabBarLabel !== undefined
+                    ? options.tabBarLabel
+                    : options.title !== undefined
+                        ? options.title
+                        : route.name;
 
-                const isFocused = state.index === index;
+            const isFocused = state.index === index;
 
-                const onPress = () => {
-                    const event = navigation.emit({
-                        type: 'tabPress',
-                        target: route.key,
-                    });
+            const onPress = () => {
+                const event = navigation.emit({
+                    type: 'tabPress',
+                    target: route.key,
+                });
 
-                    if (!isFocused && !event.defaultPrevented) {
-                        navigation.navigate(route.name);
-                    }
-                };
+                if (!isFocused && !event.defaultPrevented) {
+                    navigation.navigate(route.name);
+                }
+            };
 
-                return (
-                    <TouchableOpacity
-                        accessibilityRole="button"
-                        accessibilityStates={isFocused ? ['selected'] : []}
-                        accessibilityLabel={options.tabBarAccessibilityLabel}
-                        testID={options.tabBarTestID}
-                        onPress={onPress}
-                        style={styles.tabContainer}>
-                        <Text style={isFocused ? styles.active : styles.inactive}>{label}</Text>
-                    </TouchableOpacity>
-                );
-            })}
-        </View>
-    );
-}
+            return (
+                <TouchableOpacity
+                    accessibilityRole="button"
+                    accessibilityStates={isFocused ? ['selected'] : []}
+                    accessibilityLabel={options.tabBarAccessibilityLabel}
+                    testID={options.tabBarTestID}
+                    key={index}
+                    onPress={onPress}
+                    style={styles.tabContainer}>
+                    <Text style={isFocused ? styles.active : styles.inactive}>{label}</Text>
+                </TouchableOpacity>
+            );
+        })}
+    </View>
+);
 
-export default function HomeScreen() {
-    const defaultMatch = {
-        tournament: "Liga MX",
-        stadium: "Estadio Azteca",
-        localAsset: "pumas",
-        local_url: null,
-        localTeam: "Pumas",
-        score: "0 - 0",
-        visitorAsset: "america",
-        visitor_url: null,
-        visitTeam: "América",
-        date: "Sábado 25, ABRIL"
-    }
-    return (
-        <Wrapper>
-            <HeaderMatch match={defaultMatch} />
-            <Hometabs.Navigator tabBar={props => <HomeTab {...props} />}>
-                <Hometabs.Screen name="Posts" component={PostsScreen} />
-                <Hometabs.Screen name="Videos" component={VideosScreen} />
-                <Hometabs.Screen name="Statistics" component={StatisticsScreen} />
-                <Hometabs.Screen name="Calendar" component={CalendarScreen} />
-            </Hometabs.Navigator>
-        </Wrapper>
-    )
-}
+export default HomeScreen = () => (
+    <Wrapper>
+        <HeaderMatch match={defaultMatch} />
+        <Hometabs.Navigator tabBar={props => <HomeTab {...props} />}>
+            <Hometabs.Screen name="Posts" component={PostsScreen} />
+            <Hometabs.Screen name="Videos" component={VideosScreen} />
+            <Hometabs.Screen name="Statistics" component={StatisticsScreen} />
+            <Hometabs.Screen name="Calendar" component={CalendarScreen} />
+        </Hometabs.Navigator>
+    </Wrapper>
+)
 
 const styles = StyleSheet.create({
     tabStyle: {
