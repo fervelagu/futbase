687a1218e4f2449942d72ec5373353bf64e92e41
Fernanda Velazquez <fevelazq@gmail.com>
2020-04-16 23:02:07 -0500

    post detail screen

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Wrapper } from '../components/Wrapper';
+import { ImageBackground, StyleSheet, View, ScrollView, Text } from 'react-native';
+import { COLORS } from '../styles';
+import { BackButton } from '../components/BackButton';
 
 export default class DetailScreen extends React.Component {
-    render(){
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            post: this.props.route.params.post
+        };
+    }
+
+    render() {
+        const { img, title, description } = this.state.post;
         return (
-            <Wrapper>
-                <Text>DetailScreen.js</Text>
-            </Wrapper>
+            <View style={styles.container}>
+                <ImageBackground
+                    resizeMode="cover"
+                    source={{ uri: img }}
+                    style={styles.bg}>
+                    <BackButton />
+                </ImageBackground>
+                <ScrollView>
+                    <View style={styles.subContainer}>
+                        <Text style={styles.titleLabel}>
+                            {title}
+                        </Text>
+                        <Text style={styles.descriptionLabel}>
+                            {description}
+                        </Text>
+                    </View>
+                </ScrollView>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    bg: {
+        width: "100%",
+        height: 200,
+    },
+    container: {
+        position: "relative",
+        width: "100%",
+        height: "100%"
+    },
+    subContainer: {
+        padding: 15,
+        justifyContent: "flex-start"
+    },
+    titleLabel: {
+        color: COLORS.TEAM_PRIMARY,
+        fontSize: 22,
+        fontWeight: "bold",
+        paddingVertical: 10
+    },
+    descriptionLabel: {
+        flex: 1,
+        flexWrap: "wrap",
+        fontSize: 16
+    }
+})
