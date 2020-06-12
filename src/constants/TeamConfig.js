8f5f21609112ef852212a7e7f63e3dedf9657376
Fernanda velazquez <fevelazq@gmail.com>
2020-06-11 21:33:39 -0500

    contact screen

--- src/constants/TeamConfig.js
+++ src/constants/TeamConfig.js
@@ -1,3 +1,4 @@
 export const TEAM_NAME = "pumas";
+export const TEAM_MAIL = "mailto:fevelazq@gmail.com";
 export const DEFAULT_VIDEO = "https://www.youtube.com/embed/6Uo5vzhTsBE"
 export const HISTORY_VIDEO = "https://www.youtube.com/embed/6Uo5vzhTsBE"
--- src/screens/ContactUsScreen.js
+++ src/screens/ContactUsScreen.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { NoData } from "../components/NoData";
+import { View, Text, TouchableHighlight, StyleSheet, Linking } from 'react-native';
+import { Icon } from 'native-base';
 import { Wrapper } from '../components/Wrapper';
+import { BackButton } from '../components/BackButton';
+import { COLORS } from '../styles';
+import { TEAM_MAIL } from '../constants/TeamConfig';
 
 export default class ContactUsScreen extends React.Component {
     render() {
         return (
             <Wrapper>
-                <NoData />
+                <BackButton title={"Contacto"} />
+                <View style={styles.container}>
+                    <View style={styles.sub}>
+                        <Icon name={"md-mail"} style={styles.icon} />
+                        <Text style={styles.text}>Contáctanos en:</Text>
+                        <TouchableHighlight
+                            underlayColor="transparent"
+                            onPress={() => Linking.openURL(TEAM_MAIL)}>
+                            <Text style={[styles.text, styles.mail]}>fevelazq@gmail.com</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
             </Wrapper>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.WHITE,
+        justifyContent: "center",
+    },
+    sub: {
+        alignItems: "center",
+        alignSelf: "center"
+    },
+    text: {
+        color: COLORS.TEAM_PRIMARY,
+        fontSize: 24
+    },
+    mail: {
+        fontStyle: "italic"
+    },
+    icon: {
+        color: COLORS.TEAM_PRIMARY,
+        fontSize: 80
+    }
+})
