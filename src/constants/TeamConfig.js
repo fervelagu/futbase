2004570ffab000bcc5bb2a7a216e0b57fe3fb3ff
Fernanda velazquez <fevelazq@gmail.com>
2020-06-11 21:41:34 -0500

    anthem screen

--- src/constants/TeamConfig.js
+++ src/constants/TeamConfig.js
@@ -1,4 +1,6 @@
 export const TEAM_NAME = "pumas";
 export const TEAM_MAIL = "mailto:fevelazq@gmail.com";
+export const TEAM_HASHTAG = "#SoyDePumas";
 export const DEFAULT_VIDEO = "https://www.youtube.com/embed/6Uo5vzhTsBE"
 export const HISTORY_VIDEO = "https://www.youtube.com/embed/6Uo5vzhTsBE"
+export const ANTHEM_VIDEO = "https://www.youtube.com/embed/6Uo5vzhTsBE"
--- src/screens/AnthemScreen.js
+++ src/screens/AnthemScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { NoData } from "../components/NoData";
 import { Wrapper } from '../components/Wrapper';
+import { BackButton } from '../components/BackButton';
+import { i18n } from '../utils/i18n';
+import { Video } from '../components/Video';
+import { TEAM_HASHTAG, ANTHEM_VIDEO } from '../constants/TeamConfig';
 
 export default class AnthemScreen extends React.Component {
     render() {
         return (
             <Wrapper>
-                <NoData />
+                <BackButton title={i18n.t("Team.anthemScreen.header")} />
+                <Video
+                    title={TEAM_HASHTAG}
+                    video={ANTHEM_VIDEO}
+                />
             </Wrapper>
         )
     }
